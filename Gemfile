4f67a1eff226f2f8fc11ccf8b05862843dd36838
kt32291 <Katharine322@gmail.com>
2015-05-20 16:25:50 -0500

    first commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.21'
+
+gem 'locomotive_cms', '~> 2.5.6', :require => 'locomotive/engine'
+gem 'locomotive-heroku', :require => 'locomotive/heroku'
+gem 'figaro'
+gem 'unicorn'
+gem 'newrelic_rpm'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'compass',        '~> 0.12.7'
+  gem 'compass-rails',  '~> 2.0.0'
+end
+
+group :production do
+  gem 'thin'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RyenotEngine::Application.config.secret_token = 'b5be030aef2095fb169b41032a2960cff012e1c61a0dbff7f579b6e394fe4be0e9ff94ba11d51387fbe665e674bf3c23782acf00613e883cb6a116938ecf392d'
